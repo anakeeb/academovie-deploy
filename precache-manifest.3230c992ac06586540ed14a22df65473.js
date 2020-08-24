3b6993a8ffde8c133ebbacadf17afac5b45ceeed
anakeeb <anakeeb@purdue.edu>
2020-08-23 20:38:40 -0400

    Updates

---
+++ precache-manifest.3230c992ac06586540ed14a22df65473.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e856fedf4687c2fc197323472ece28ad",
+    "url": "/academovie-deploy/index.html"
+  },
+  {
+    "revision": "ad50a37ac8a864e9c969",
+    "url": "/academovie-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40a70f7ed079365082f7",
+    "url": "/academovie-deploy/static/js/2.18a7ef49.chunk.js"
+  },
+  {
+    "revision": "2d288655e29bfcf0df0c3b80145be6e2",
+    "url": "/academovie-deploy/static/js/2.18a7ef49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad50a37ac8a864e9c969",
+    "url": "/academovie-deploy/static/js/main.63beedc0.chunk.js"
+  },
+  {
+    "revision": "f3c3dcddb3d24548ae51",
+    "url": "/academovie-deploy/static/js/runtime-main.3ea77e17.js"
+  },
+  {
+    "revision": "e7bbefb26dc95fef52bbbd957b0018e2",
+    "url": "/academovie-deploy/static/media/filmRoll.e7bbefb2.png"
+  },
+  {
+    "revision": "97431553d8f515eaa438ab2552121d18",
+    "url": "/academovie-deploy/static/media/filmRollOpaque.97431553.png"
+  },
+  {
+    "revision": "6e0c9aaee36946b968d2f029c1e7bd14",
+    "url": "/academovie-deploy/static/media/movieLogo.6e0c9aae.png"
+  }
+]);
